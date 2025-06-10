d375a10d68b9fa01977de889fa55803b41642aa6
Đăng Long Nguyễn <nguyenlongdang04121999@gmail.com>
2025-06-10 14:21:49 +0700

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
-import { Elysia } from "elysia";
+// ** Elysia Imports
+import { cors } from '@elysiajs/cors'
+import { swagger } from '@elysiajs/swagger'
+import { Elysia } from 'elysia'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia({ prefix: '/api', normalize: true })
+    .use(swagger())
+    .use(
+        cors({
+            credentials: true,
+            origin: [Bun.env.USER_URL!],
+            allowedHeaders: ['Content-Type', 'Authorization']
+        })
+    )
+    .get('/', () => 'Hello Elysia')
+    .listen(Bun.env.PORT || 3333)
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`)
