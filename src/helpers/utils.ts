66d560bc64b89ea496185f6b7cb5d72d391d1855
Đăng Long Nguyễn <nguyenlongdang04121999@gmail.com>
2025-06-18 17:16:35 +0700

    generate username

---
+++ src/helpers/utils.ts
@@ -0,0 +1,34 @@
+// ** Prisma Imports
+import prisma from '@db';
+
+export async function generateUsername(name: string, email: string): Promise<string> {
+    let base = name
+        .normalize('NFD')
+        .replace(/[\u0300-\u036f]/g, '')
+        .toLowerCase()
+        .replace(/[^a-z0-9._-]+/g, '')
+
+    if (!base || base.length < 3) {
+        base = email.split('@')[0]
+    }
+
+    let username = base
+    let count = 0
+    let existed = await prisma.user.findUnique({
+        where: {
+            username
+        }
+    })
+
+    while (existed) {
+        count++
+        username = base + count
+        existed = await prisma.user.findUnique({
+            where: {
+                username
+            }
+        })
+    }
+
+    return username
+}
---
+++ src/users/auth/services/authRegister.service.ts
@@ -0,0 +1,68 @@
+// ** Elysia Imports
+import { Elysia } from 'elysia';
+
+// ** Prisma Imports
+import prisma from '@db';
+import { AuthProvider } from '@prisma/client';
+
+// ** Constants Imports
+import { HASH_PASSWORD } from '@constants';
+import { ERROR_CODES } from '@constants/errorCodes';
+
+// ** Models Imports
+import { authModels } from '../auth.model';
+
+// ** Helpers Imports
+import { generateUsername } from '@helpers/utils';
+
+export const authRegister = new Elysia()
+    .use(authModels)
+    .post(
+        '/register',
+        async ({ body, status }) => {
+            const { email, name, password } = body
+
+            const exist = await prisma.user.findUnique({
+                where: { email }
+            })
+
+            if (exist) {
+                return status('Conflict', {
+                    code: ERROR_CODES.EMAIL_EXISTS,
+                    message: 'This email is already in use'
+                })
+            }
+
+            const username = await generateUsername(name, email)
+
+            const hashed = await Bun.password.hash(password, HASH_PASSWORD.ALGORITHM)
+
+            const user = await prisma.user.create({
+                data: {
+                    email,
+                    name,
+                    username,
+                    password: hashed,
+                    provider: AuthProvider.LOCAL,
+                    isVerified: false,
+                },
+                select: {
+                    id: true,
+                    email: true,
+                    name: true,
+                    username: true,
+                    provider: true,
+                    isVerified: true,
+                    role: true,
+                    createdAt: true
+                }
+            })
+
+            return status('OK', {
+                data: user
+            })
+        },
+        {
+            body: 'register'
+        }
+    )
