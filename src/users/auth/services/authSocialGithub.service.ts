e2a07dbe7b97cfa5633d5ecf1867ec0d1f10bcd0
Đăng Long Nguyễn <nguyenlongdang04121999@gmail.com>
2025-06-18 17:21:33 +0700

    return username

--- src/users/auth/services/authSocialGithub.service.ts
+++ src/users/auth/services/authSocialGithub.service.ts
@@ -151,6 +151,7 @@ export const authSocialGithub = new Elysia()
                     id: user.id,
                     email: user.email,
                     name: user.name,
+                    username: user.username,
                     avatar: user.avatar,
                     role: user.role,
                     provider: user.provider
--- src/users/auth/services/authSocialGoogle.service.ts
+++ src/users/auth/services/authSocialGoogle.service.ts
@@ -143,6 +143,7 @@ export const authSocialGoogle = new Elysia()
                     id: user.id,
                     email: user.email,
                     name: user.name,
+                    username: user.username,
                     avatar: user.avatar,
                     role: user.role,
                     provider: user.provider
