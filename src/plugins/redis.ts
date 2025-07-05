ee92cb71da9c9faf859b876df565f18781b090f5
nguyenlongdang0412 <nguyenlongdang04121999@gmail.com>
2025-07-05 21:43:11 +0700

    setup redis

---
+++ src/plugins/redis.ts
@@ -0,0 +1,59 @@
+// ** Elysia Imports
+import { Elysia } from 'elysia'
+
+// ** Third Party Imports
+import Redis from 'ioredis'
+
+class RedisClient {
+    private redisClient: Redis
+    private expiresAt = 60 * 60
+
+    constructor(_connectionString?: string) {
+        this.redisClient = _connectionString ? new Redis(_connectionString) : new Redis()
+
+        this.redisClient.on('error', (error) => {
+            console.error('[Redis] Error:', error)
+        })
+
+        this.redisClient.on('close', () => {
+            console.error('[Redis] Connection closed.')
+        })
+    }
+
+    async set(key: string, value: string, expiresAt = this.expiresAt) {
+        return this.redisClient.set(key, value, 'EX', expiresAt)
+    }
+
+    async get(key: string) {
+        return this.redisClient.get(key)
+    }
+
+    async del(key: string) {
+        return this.redisClient.del(key)
+    }
+
+    async forgetAll() {
+        return this.redisClient.flushall()
+    }
+
+    async sadd(key: string, value: any) {
+        return this.redisClient.sadd(key, value)
+    }
+
+    async smembers(key: string) {
+        return this.redisClient.smembers(key)
+    }
+
+    async srem(key: string, value: string) {
+        return this.redisClient.srem(key, value)
+    }
+}
+
+const redisPlugin = (app: Elysia) =>
+    app.decorate({
+        redis: new RedisClient(Bun.env.REDIS_URL)
+    })
+
+export { redisPlugin }
+
+export type RedisClientType = InstanceType<typeof RedisClient>
